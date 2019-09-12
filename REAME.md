c15645425a3fd5331cfa8cf17d373eb6ac36e6b5
Liang Chun Wong <liang-chun.wong@trivago.com>
2019-09-12 14:17:34 +0200

    feat(webpack): set up webpack build

---
+++ REAME.md
@@ -0,0 +1,16 @@
+# js-template
+
+## Quick Start
+
+Make sure `yarn` is installed.
+
+```sh
+# install dependencies
+yarn
+
+# start the development server
+yarn start
+
+# production build
+yarn build
+```
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin').CleanWebpackPlugin
+
+module.exports = {
+    mode: process.env.NODE_ENV,
+    entry: "./src/index.js",
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+        contentBase: './dist',
+    },
+    module: {
+        rules: [
+          { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, './public/index.html')
+        })
+    ]
+}
