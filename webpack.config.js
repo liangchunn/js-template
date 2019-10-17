22df0de9b5cc0abaaefe403ecd0fb8ed93906c04
Liang Chun <liangchunn@me.com>
2019-10-17 14:44:15 +0200

    feat(webpack): use @babel/preset-env and add prettier config

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,33 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const CleanWebpackPlugin = require('clean-webpack-plugin').CleanWebpackPlugin
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const CleanWebpackPlugin = require("clean-webpack-plugin").CleanWebpackPlugin
 
 module.exports = {
-    mode: process.env.NODE_ENV,
-    entry: "./src/index.js",
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    devServer: {
-        contentBase: './dist',
-    },
-    module: {
-        rules: [
-          { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
-        ]
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, './public/index.html')
-        })
-    ]
+  mode: process.env.NODE_ENV,
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    contentBase: "./dist",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/preset-env"],
+        },
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "./public/index.html"),
+    }),
+  ],
 }
